0e9ceade05b61a45ea6c28eb4aa9a1f3a8150930
ninjabattler <jaydenrtucker@gmail.com>
2020-08-26 22:56:30 +0000

    Fixed the function so that it outputs false if there is no word and can search for words vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function (matrix) {
 
-module.exports = wordSearch
+    const resultArray = []
+
+    for (let row = 0; row < matrix[0].length; row++) {
+        let tempArray = [];
+        for (let col = 0; col < matrix.length; col++) {
+            tempArray.push(matrix[col][row]);
+        }
+        resultArray.push(tempArray);
+    }
+    return resultArray;
+
+};
+
+const wordSearch = (letters, word) => {
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    const horizontalJoin = letters.map(ls => ls.join(''))//"LOVEQUAL"
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {  // love === lovequal => false
+            return true
+        }
+    }
+    for(l of verticalJoin){
+        if(l.includes(word)){
+            return true
+        }
+    }
+    return false;
+}
+module.exports = wordSearch;
